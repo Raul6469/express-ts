00de503d4d60abe03353957457c53041ba4bcd97
Victor <vb4007@gmail.com>
2018-06-23 10:15:27 +0200

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
