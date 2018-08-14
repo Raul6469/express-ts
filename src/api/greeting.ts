f1c434549e83250c334ef9e033d48babe401a992
Victor <vb4007@gmail.com>
2018-08-14 23:38:45 +0200

    Reuse Request type

--- src/api/greeting.ts
+++ src/api/greeting.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 
-export let hello = function(req: any, res: Response) {
+export let hello = function(req: Request, res: Response) {
     res.send({
         message: 'Hello ' + req.user.username + '!' 
     })
